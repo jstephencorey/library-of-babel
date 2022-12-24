a051b4e46de875505e7ed8a6f46865ac9eefd1fb
jstephencorey <Yosef.d.kori@gmail.com>
2022-12-24 15:22:43 -0700

    Further changes

--- main.py
+++ main.py
@@ -10,9 +10,18 @@ def get_integer_input(message):
         except:
             continue
 
+# def get_option():
+#     print("1: Get text of a page")
+#     print("2: Get the number of a page containing a certain text")
+#     while True:
+#         inp = input()
+#         if inp in ('1','2'):
+#             return 
+
 if __name__ == "__main__":
     lib = library()
-    print("Hello, welcome to the Library of Babel, do you want to look up a book?")
+    print("Hello, welcome to the Library of Babel, Here are your options:")
+    # option = get_option()
     page_num = get_integer_input("What page number do you want?")
     print("Your page contains:")
     page = lib.get_page(page_num)
